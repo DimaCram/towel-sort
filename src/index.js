067950fb44c855be43e50472253ec6397bdc09c9
Dmitrij <kramkov@ncpi.gov.by>
2020-10-06 09:43:00 +0300

    task check

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(typeof matrix === 'undefined') return [];
+
+  let rez = [];
+  matrix.forEach(function(element, index, array){
+    let arr = (index + 1) % 2 === 0 ? element.reverse() : element;
+    rez.push(...arr);
+  });
+  return rez;
 }
